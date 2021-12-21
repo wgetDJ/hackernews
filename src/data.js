914b53bbe6442f5f0e08cf8ab3b7232798d54a26
Dibyajyoti Mishra <dibyajyotimishraiiit@gmail.com>
2021-12-21 23:44:17 +0530

    FEAT: fake data to simulate API

---
+++ src/data.js
@@ -0,0 +1,10 @@
+export default [
+    {
+        title: "PR to Merge Multicore OCaml",
+        link: "github.com/ocaml",
+        points: 178,
+        by: "c-cube",
+        time: "2 hours ago",
+        comments: 38
+    }
+]
