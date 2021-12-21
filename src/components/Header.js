4b18ef0ec29433d8f3a164f5b7d6ad992dd79834
Dibyajyoti Mishra <dibyajyotimishraiiit@gmail.com>
2021-12-21 23:38:05 +0530

    FEAT: base design

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div className = "header">
+            <div className = "logo">
+                <img src = "logo-body.svg"/>
+            </div>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/components/PostCard.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const PostCard = () => {
+    return (
+        <div className = "post--card"></div>
+    )
+}
+
+export default PostCard
